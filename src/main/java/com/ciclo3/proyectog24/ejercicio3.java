c7f1075696c7a979ccbbd261cc226e55b1129cc2
Richard Mahecha <richard.mahecha.mt@usa.edu.co>
2022-09-14 18:39:53 -0500

    Agregamos el ejercicio3

---
+++ src/main/java/com/ciclo3/proyectog24/ejercicio3.java
@@ -0,0 +1,14 @@
+package com.ciclo3.proyectog24;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RICHARD
+ */
+public class ejercicio3 {
+    
+}
